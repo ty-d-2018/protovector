c3eeb86f2178fcbbb2612dea3a6bb29ef1515074
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-09-20 17:55:57 -0600

    Made a type alias because I don't know what data type to go with the vector trait yet.

--- src/vector.rs
+++ src/vector.rs
@@ -1,46 +1,47 @@
 pub type DynVector = dyn Vector;
 pub type BoxedVector = Box::<DynVector>;
+pub type Numeric = f32;
 
 pub trait Vector{
-    fn get_dimension(&self) -> (f32, f32, f32);
+    fn get_dimension(&self) -> (Numeric, Numeric, Numeric);
     fn get_clone(&self) -> BoxedVector;
-    fn get_x(&self) -> f32{
+    fn get_x(&self) -> Numeric{
         let (x, y, z) = self.get_dimension();
         x
     }
-    fn get_y(&self) -> f32{
+    fn get_y(&self) -> Numeric{
         let (x, y, z) = self.get_dimension();
         y
     }
-    fn get_z(&self) -> f32{
+    fn get_z(&self) -> Numeric{
         let(x, y, z) = self.get_dimension();        
         z
     }
-    fn get_xy(&self) -> (f32, f32){
+    fn get_xy(&self) -> (Numeric, Numeric){
         let(x, y, z) = self.get_dimension();
         (x, y)
     }
-    fn get_xz(&self) -> (f32, f32){
+    fn get_xz(&self) -> (Numeric, Numeric){
         let (x, y, z) = self.get_dimension();
         (x, z)
     }
-    fn get_yx(&self) -> (f32, f32){
+    fn get_yx(&self) -> (Numeric, Numeric){
         let (x, y, z) = self.get_dimension();
         (y, x)
     }
-    fn get_yz(&self) -> (f32, f32){
+    fn get_yz(&self) -> (Numeric, Numeric){
         let (x, y, z) = self.get_dimension();
         (y, z)
     }
-    fn get_zx(&self) -> (f32, f32){
+    fn get_zx(&self) -> (Numeric, Numeric){
         let (x, y, z) = self.get_dimension();
         (z, x)
     }
-    fn get_zy(&self) -> (f32, f32){
+    fn get_zy(&self) -> (Numeric, Numeric){
         let (x, y, z) = self.get_dimension();
         (z, y)
     }
-    fn get_matrix(&self) -> Vec<f32>{
+    fn get_matrix(&self) -> Vec<Numeric>{
         todo!();
     }
 }
