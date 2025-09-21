e20822bb30715be645bcd3a9e86e6e1c84a69469
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-09-20 19:50:06 -0600

    finished the basic operations of SimpleMesh trait

--- src/linear.rs
+++ src/linear.rs
@@ -14,5 +14,17 @@ pub mod mesh{
 
             (left_x + right_x, left_y + right_y, left_z + right_z)
         }
+        fn divide(left: &BoxedVector, right: &BoxedVector) -> (Numeric, Numeric, Numeric){
+            let (left_x, left_y, left_z) = left.get_dimension();
+            let (right_x, right_y, right_z) = right.get_dimension();
+
+            (left_x / right_x, left_y / right_y, left_z / right_z)
+        }
+        fn subtract(left: &BoxedVector, right: &BoxedVector) -> (Numeric, Numeric, Numeric){
+            let (left_x, left_y, left_z) = left.get_dimension();
+            let (right_x, right_y, right_z) = right.get_dimension();
+
+            (left_x - right_x, left_y - right_y, left_z - right_z)
+        }
     }
 }
