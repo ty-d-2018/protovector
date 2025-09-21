b1de61994d9d5c7ec6d3ecc78339380ca08eda08
ty-d-2018 <40513477+ty-d-2018@users.noreply.github.com>
2025-09-20 20:39:24 -0600

    moved inflect to a sub module within linear.rs source file

--- src/arrow.rs
+++ src/arrow.rs
@@ -3,25 +3,7 @@ use super::vector::{
     BoxedVector,
     Numeric,
 };
-
-#[derive(Clone)]
-pub enum Inflect{
-    X,
-    Y,
-    Z,
-    XY,
-    XZ,
-    YX,
-    YZ,
-    ZX,
-    ZY,
-    XYZ,
-    XZY,
-    YXZ,
-    ZXY,
-    YZX,
-    ZYX,
-}
+use super::linear::Sort::{ Inflect };
 
 #[derive(Clone)]
 pub struct Direction{
@@ -64,73 +46,6 @@ impl Direction{
         Direction{
             Inflect: Inflect.clone(),
             scalar: point.clone(),
-        }
-    }
-}
-
-impl Inflect{
-    pub fn get_scalar(&self, vector: &BoxedVector) -> (Numeric, Numeric, Numeric){
-        match self{
-            Inflect::X => {
-                let x = vector.get_x();
-                (x, 0.0, 0.0)
-            },
-            Inflect::Y => {
-                let y = vector.get_y();
-                (y, 0.0, 0.0)
-            },
-            Inflect::Z => {
-                let z = vector.get_z();
-                (z, 0.0, 0.0)
-            },
-            Inflect::XY => {
-                let (x, y) = vector.get_xy();
-                (x, y, 0.0)
-            },
-            Inflect::XZ => {
-                let (x, z) = vector.get_xz();
-                (x, z, 0.0)
-            },
-            Inflect::YX => {
-                let (y, x) = vector.get_yx();
-                (y, x, 0.0)
-            },
-            Inflect::YZ => {
-                let (y, z) = vector.get_yz();
-                (y, z, 0.0)
-            }
-            Inflect::ZX => {
-                let (z, x) = vector.get_zx();
-                (z, x, 0.0)
-            },
-            Inflect::ZY => {
-                let (z, y) = vector.get_yz();
-                (z, y, 0.0)
-            },
-            Inflect::XYZ => {
-                let (x, y, z) = vector.get_dimension();
-                (x, y, z)
-            },
-            Inflect::XZY => {
-                let (x, y, z) = vector.get_dimension();
-                (x, z, y)
-            }
-            Inflect::YXZ => {
-                let (x, y, z) = vector.get_dimension();
-                (y, x, z)
-            },
-            Inflect::ZXY => {
-                let (x, y, z) = vector.get_dimension();
-                (z, x, y)
-            },
-            Inflect::YZX => {
-                let (x, y, z) = vector.get_dimension();
-                (y, z, x)
-            }
-            Inflect::ZYX => {
-                let (x, y, z) = vector.get_dimension();
-                (z, y, x)
-            }
         }
     }
 }
--- src/linear.rs
+++ src/linear.rs
@@ -35,3 +35,93 @@ pub enum Operation{
     Subtract,
     Divide,
 }
+
+pub mod Sort{
+    use super::super::vector::{ Vector, BoxedVector, Numeric };
+
+    #[derive(Clone)]
+    pub enum Inflect{
+        X,
+        Y,
+        Z,
+        XY,
+        XZ,
+        YX,
+        YZ,
+        ZX,
+        ZY,
+        XYZ,
+        XZY,
+        YXZ,
+        ZXY,
+        YZX,
+        ZYX,
+    }
+    
+    impl Inflect{
+        pub fn get_scalar(&self, vector: &BoxedVector) -> (Numeric, Numeric, Numeric){
+            match self{
+                Inflect::X => {
+                    let x = vector.get_x();
+                    (x, 0.0, 0.0)
+                },
+                Inflect::Y => {
+                    let y = vector.get_y();
+                    (y, 0.0, 0.0)
+                },
+                Inflect::Z => {
+                    let z = vector.get_z();
+                    (z, 0.0, 0.0)
+                },
+                Inflect::XY => {
+                    let (x, y) = vector.get_xy();
+                    (x, y, 0.0)
+                },
+                Inflect::XZ => {
+                    let (x, z) = vector.get_xz();
+                    (x, z, 0.0)
+                },
+                Inflect::YX => {
+                    let (y, x) = vector.get_yx();
+                    (y, x, 0.0)
+                },
+                Inflect::YZ => {
+                    let (y, z) = vector.get_yz();
+                    (y, z, 0.0)
+                }
+                Inflect::ZX => {
+                    let (z, x) = vector.get_zx();
+                    (z, x, 0.0)
+                },
+                Inflect::ZY => {
+                    let (z, y) = vector.get_yz();
+                    (z, y, 0.0)
+                },
+                Inflect::XYZ => {
+                    let (x, y, z) = vector.get_dimension();
+                    (x, y, z)
+                },
+                Inflect::XZY => {
+                    let (x, y, z) = vector.get_dimension();
+                    (x, z, y)
+                }
+                Inflect::YXZ => {
+                    let (x, y, z) = vector.get_dimension();
+                    (y, x, z)
+                },
+                Inflect::ZXY => {
+                    let (x, y, z) = vector.get_dimension();
+                    (z, x, y)
+                },
+                Inflect::YZX => {
+                    let (x, y, z) = vector.get_dimension();
+                    (y, z, x)
+                }
+                Inflect::ZYX => {
+                    let (x, y, z) = vector.get_dimension();
+                    (z, y, x)
+                }
+            }
+        }
+    }
+}
